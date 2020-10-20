20bf15da4ac98a6e0e90816cebe68bd552119896
ajay812998 <ajay812998@gmail.com>
2020-10-21 00:46:00 +0530

    Added : Checking Emp Present or Absent

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -3,6 +3,16 @@ package com.bridgelabz;
 public class Employee{
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Problem");
+			int present=1;
+			int checkout=(int) (Math.floor(Math.random() *10) %2);
+			System.out.println(checkout);
+			if (checkout == present) {
+				System.out.println("Employee Present");
+			}
+			else
+				System.out.println("Employee Absent");
+			
+			
+			
 	}
 }
