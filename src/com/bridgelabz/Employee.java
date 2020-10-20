b140cefd4ed80ff2dcb6cea98bd882ca82be6a13
ajay812998 <ajay812998@gmail.com>
2020-10-21 01:23:41 +0530

    Added : Using Switch Case Statement

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -10,18 +10,18 @@ public class Employee{
 		int Part_Day_Hour=4;
 		int empcheckout=(int) (Math.floor(Math.random() *10) %3);
 		System.out.println("The Result is :"+empcheckout);
-		if (empcheckout == Full_Time) {
-			int Total_Wages=Wages_Per_Hour * full_Day_Hour;
-			System.out.println("Total Wages for Full Time is :"+Total_Wages);
+		switch(empcheckout){
+		    case 0 : System.out.println("Employee is Absent");
+                        break;
+			case 1 :int total_Wages=Wages_Per_Hour * full_Day_Hour;
+						System.out.println("Wages for Full Time is :"+total_Wages);
+						break;
+			case 2 :int total_part_time=Wages_Per_Hour * Part_Day_Hour;
+						System.out.println("Wages for Part Time Is :"+total_part_time);
+						break;
+			default : System.out.println("Wrong Entry");
+		                break;
 		}
-		else if (Part_Time == empcheckout) {
-			int total_Wages=Part_Day_Hour * Wages_Per_Hour;
-			System.out.println("Total Wages for Part Time Is :"+total_Wages);
-		}
-		else {
-			  System.out.println("Employee is Absent");
-		}
-	}
-
+		
+    }
 }
-
