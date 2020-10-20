1c25eecebf46896c1f9eceddaacee4584366e3b9
ajay812998 <ajay812998@gmail.com>
2020-10-21 01:55:51 +0530

    Added : Calculate Wages till Condition Reached

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -9,18 +9,29 @@ public class Employee{
 		int full_Day_Hour=8;
 		int Part_Day_Hour=4;
 		int month_days=20;
-		int empcheckout=(int) (Math.floor(Math.random() *10) %3);
+		int workingDays=0;
+		int workingHour=1;
+		int Max_days=20;
+		int Total=0;
+		while (workingDays <= Max_days && workingHour <= 100) {
+		int empcheckout=(int) (Math.floor(Math.random() *10) %3 );
 		System.out.println("The Result is :"+empcheckout);
 		switch(empcheckout){
 		    case 0 :System.out.println("Employee is Absent");
 		                   break;
-			case 1 :int total=Wages_Per_Hour * full_Day_Hour;
-								System.out.println("Wage for Full Time is :"+total * month_days);
+			case 1 :int total_wage=Wages_Per_Hour * full_Day_Hour;
+								workingHour=workingHour+full_Day_Hour;
+								Total=total_wage+Total;
 								break;
 			case 2 :int total_part_time=Wages_Per_Hour * Part_Day_Hour;
-								System.out.println("Wage for Part Time is :"+total_part_time * month_days);
+								workingHour=workingHour+Part_Day_Hour;
+								Total=total_part_time+Part_Day_Hour;
 								break;
 			default : System.out.println("Wrong Entry");
+			                    break;
 		}
+	   workingDays++;
+	}
+     System.out.println("Wages in a Month are :"+Total);
 	}
 }
