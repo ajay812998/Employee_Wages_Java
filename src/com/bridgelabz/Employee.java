128cac9454f8671010108f367b92f23939d32da5
ajay812998 <ajay812998@gmail.com>
2020-10-21 00:57:01 +0530

    Added : Calculate Daily Wage

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -3,16 +3,21 @@ package com.bridgelabz;
 public class Employee{
 	
 	public static void main(String[] args) {
-			int present=1;
-			int checkout=(int) (Math.floor(Math.random() *10) %2);
-			System.out.println(checkout);
-			if (checkout == present) {
-				System.out.println("Employee Present");
-			}
-			else
-				System.out.println("Employee Absent");
-			
-			
-			
+		int Present=1;
+		int Wages_Per_Hour=20;
+		int Full_Day_Hour=8;
+		int empcheckout=(int) (Math.floor(Math.random() *10) %2);
+		System.out.println("Result is :"+empcheckout);
+		if (empcheckout == Present) {
+			int Total_Daily_Wages=Wages_Per_Hour * Full_Day_Hour;
+			System.out.println("Daily total Wages are :"+Total_Daily_Wages);
+		}
+		else
+			System.out.println("Employee is Absent");
+		
+		
+		
+
+		}
+
 	}
-}
