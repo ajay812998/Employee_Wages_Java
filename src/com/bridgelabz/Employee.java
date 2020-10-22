968473185c87adec09dc7bdfa50515e416d7764c
ajay812998 <70196133+ajay812998@users.noreply.github.com>
2020-10-23 00:51:02 +0530

    Update Employee.java

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -7,12 +7,13 @@ public class Employee{
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
 	public static final int MAX_HRS_IN_MONTH = 100;
-
-	public static void main(String[] args) {
-		int empHrs = 0;
+	
+	    int empHrs = 0;
 		int totalSalary = 0;
 		int totalEmpHrs = 0;
 		int totalWorkingDays = 0;
+		
+		public void empWageForMonth() {
 		while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
 			totalWorkingDays++;
 			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
@@ -33,4 +34,9 @@ public class Employee{
 		totalSalary = EMP_RATE_PER_HOUR * totalEmpHrs;
 		System.out.println("Total Salary is : " + totalSalary);
 	}
+public static void main(String[] args) {
+	 Employee employee= new Employee();
+     employee.empWageForMonth();
+  }
 }
+
