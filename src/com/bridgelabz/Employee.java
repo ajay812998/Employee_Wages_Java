11fd88b1ded7fb21887c289925fe6d4bd926cfa8
ajay812998 <ajay812998@gmail.com>
2020-10-25 17:55:08 +0530

    Added : Ability to Save Wage

--- src/com/bridgelabz/Employee.java
+++ src/com/bridgelabz/Employee.java
@@ -1,48 +1,55 @@
 package com.bridgelabz;
 
 public class Employee{
+
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
-	
 	private final String company;
-	private final int empRatePerHour;
-	private final int numOfworkingDays;
-	private final int maxHoursPerMonth;
+	private final int RATE_PER_HOUR;
+	private final int workingDays;
+	private final int maxHours;
+	private int totalEmpWages; 
 	
-	public Employee(String company,int empRatePerHour,int numOfworkingDays,int maxHoursPerMonth) {
+	public Employee(String company,int RATE_PER_HOUR,int workingDays,int maxHours) {
 		this.company=company;
-		this.empRatePerHour=empRatePerHour;
-		this.numOfworkingDays=numOfworkingDays;
-		this.maxHoursPerMonth=maxHoursPerMonth;
+		this.RATE_PER_HOUR=RATE_PER_HOUR;
+		this.workingDays=workingDays;
+		this.maxHours=maxHours;
 		}
-	private int function() {
+	public void Calculation() {
 		int empHours=0,totalEmpHours=0,totalWorkingDays=0;
 		
-		while(totalEmpHours<=maxHoursPerMonth && totalWorkingDays<numOfworkingDays) {
+		while(totalEmpHours<=maxHours && totalWorkingDays<workingDays) {
 			totalWorkingDays++;
 			int check=(int) (Math.floor(Math.random()*10)%3);
 			switch(check) {
-			 case IS_FULL_TIME : empHours=8;
-			 					 break;
-			 case IS_PART_TIME : empHours=4;
-			 					 break;
-			 default : empHours=0;
+			 case IS_FULL_TIME :
+				 empHours=8;
+			 	 break;
+			 case IS_PART_TIME : 
+				 empHours=4;
+			 	 break;
+			 default :
+				 empHours=0;
 			}
 			totalEmpHours+=empHours;
-			System.out.println("Day :"+totalWorkingDays+"Emp Hours :"+empHours);
+			//System.out.println("Day :"+totalWorkingDays+"Emp Hours :"+empHours);
 			
 		}
-		System.out.println("Total Employee work Hours :"+totalEmpHours);
-		return totalEmpHours;
+		//System.out.println("Total Employee work Hours :"+totalEmpHours);
+		totalEmpWages=totalEmpHours*RATE_PER_HOUR;
+	}
+	
+	@Override
+	public String toString() {
+		return "Total Emp Wage for Company :"+company+"is:"+totalEmpWages;
 	}
 	public static void main(String[] args) {
-		Employee first=new Employee("Idea",15,20,125);
-		Employee second=new Employee("Airtel",15,10,150);
-	    System.out.println("Total Employee Wage for "+first.company+":"+first.function());
-	    System.out.println("Total Employee Wage for "+second.company+":"+second.function());
- }
+		Employee Idea=new Employee("Idea",20,15,125);
+		Employee Airtel=new Employee("Airtel",30,10,150);
+		Idea.Calculation();
+		System.out.println(Idea);
+		Airtel.Calculation();
+		System.out.println(Airtel);
+	 }
 }
-
-
-
-
